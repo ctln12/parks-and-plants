0816905db47b227c5854a2a543809f07616d83f1
Cataline Baert <snuf_kin@yahoo.com>
2021-03-18 16:59:59 +0100

    create Tag and PlantTag models

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,5 +1,19 @@
 class Plant < ApplicationRecord
   belongs_to :garden
+  has_many :plant_tags, dependent: :destroy
+  has_many :tags, through: :plant_tags
 
   validates :name, :image_url, presence: true
 end
+
+# plant = Plant.new(....)
+# plant.name
+# plant.image_url
+# plant.garden_id # => id
+# plant.garden # <#Garden id=1, name="My zen garden">
+# plant.garden.name # => My zen garden
+# plant.tags # => [<#Tag id=1, name="Orchids">, <#Tag id=2, name="Flowers">]
+
+# <#Tag id=1, name="Orchids">
+# <#Tag id=2, name="Flowers">
+# <#Tag id=3, name="Cactus">
